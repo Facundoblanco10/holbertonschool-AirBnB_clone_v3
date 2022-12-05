d716b9bf463fd386353a7aeba33db6d6cba21d80
Facub10 <facub1011@gmail.com>
2022-12-05 18:19:17 +0000

    preparing task4

---
+++ api/v1/app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""""""
+
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+
+app = Flask()
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def remove():
+    storage.close()
+
+
+if __name__ == "__main__":
+    api_host = getenv("HBNB_API_HOST", default="0.0.0.0")
+    api_port = getenv("HBNB_API_PORT", default="5000")
+    app.run(host=api_host, port=api_port, threaded=True)
