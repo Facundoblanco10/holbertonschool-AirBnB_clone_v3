e42347ea9ae3789fb7157cd1deb8ac3f757753ca
Facub10 <facub1011@gmail.com>
2022-12-07 02:21:55 +0000

    task 6 first commit

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,6 +5,7 @@ from flask import Flask
 from models import storage
 from api.v1.views import app_views
 from os import getenv
+from flask import make_response, jsonify
 
 
 app = Flask(__name__)
@@ -14,6 +15,12 @@ app.register_blueprint(app_views)
 @app.teardown_appcontext
 def close(exit):
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    response = jsonify({'error': 'Not found'})
+    return make_response(response, 404)
 
 
 if __name__ == "__main__":
