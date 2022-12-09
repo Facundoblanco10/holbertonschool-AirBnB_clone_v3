4907705aba17b732c462466125d740bc08148a0a
Facub10 <facub1011@gmail.com>
2022-12-09 02:17:25 +0000

    updating task 7

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """States"""
 
+from flask import make_response, jsonify
 from api.v1.views import app_views
 import json
 from models import storage
@@ -12,3 +13,12 @@ def index():
     states = storage.all(State).values()
     states_list = list(map(lambda state: state.to_dict(), states))
     return (json.dumps(states_list))
+
+
+@app_views.route("/states/<state_id>")
+def get(state_id):
+    states = storage.all(State).values()
+    for state in states:
+        if state.id == state_id:
+            return json.dumps(state.to_dict())
+    return make_response(jsonify({'error': 'Not found'}), 404)
