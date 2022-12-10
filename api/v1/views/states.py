57d14b380b3d1e16d8231117e0f64e8d694123df
Facundo10 <facub1011@gmail.com>
2022-12-10 02:29:11 +0000

    Task 7 methods

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """States"""
 
-from flask import make_response, jsonify
+from flask import make_response, jsonify, request
 from api.v1.views import app_views
 import json
 from models import storage
@@ -21,4 +21,41 @@ def get(state_id):
     for state in states:
         if state.id == state_id:
             return json.dumps(state.to_dict())
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+@app_views.route("/states/<state_id>", methods=['DELETE'])
+def delete(state_id):
+    states = storage.all(State).values()
+    for state in states:
+        if state.id == state_id:
+            state.delete()
+            storage.save()
+            return make_response(jsonify({}), 200)
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+@app_views.route("/states", methods=['POST'])
+def post():
+    try:
+        data = request.get_json()
+    except ValueError:
+        return make_response(jsonify({'message': 'Not a JSON'}), 400)
+    if not 'name' in data:
+        return make_response(jsonify({'message': 'Missing name'}), 400)
+    state = State()
+    state.name = data['name']
+    storage.new(state)
+    storage.save()
+    return make_response(state.to_dict(), 201)
+
+
+@app_views.route("/states/<state_id>", methods=['PUT'])
+def put(state_id):
+    states = storage.all(State).values()
+    for state in states:
+        if state.id == state_id:
+            state.name = request.get_json()['name']
+            storage.save()
+            return make_response(state.to_dict(), 200)
     return make_response(jsonify({'error': 'Not found'}), 404)
